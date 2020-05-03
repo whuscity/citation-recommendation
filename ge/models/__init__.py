ce05a7ee5d4f9756200b540f19676fdc57c869d8
flyingzhy <38338733+flyingzhy@users.noreply.github.com>
2020-05-03 11:06:49 +0800

    模型文件

---
+++ ge/models/__init__.py
@@ -0,0 +1,9 @@
+from .deepwalk import DeepWalk
+# from .node2vec import Node2Vec
+from .line import LINE
+from .node2vec import Graph
+# from .sdne import SDNE
+# from .struc2vec import Struc2Vec
+
+
+__all__ = ["DeepWalk", "LINE","Graph"]
